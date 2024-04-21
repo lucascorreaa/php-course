b83682b7542d40bd478a151b9ba191a71fb5b450
lucascorreaa <lucas.lca14@gmail.com>
2024-04-21 14:09:47 -0300

    feat: variables in php, store and access data dynamically

---
+++ first-step/04-variables/page.php
@@ -0,0 +1,14 @@
+<?php 
+    $title = 'PHP is amazing!!!';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title ?></title>
+</head>
+<body>
+    <h1><?php echo $title ?></h1>
+</body>
+</html>
---
+++ first-step/04-variables/variables.php
@@ -0,0 +1,9 @@
+<?php 
+
+$greeting = 'Hello PHP on Brazil!';
+$year = 2030;
+
+echo $greeting;
+echo $year;
+
+?>
