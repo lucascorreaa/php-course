7600f51c9ac764b24f47239cd1ce5b3e95a78ba0
lucascorreaa <lucas.lca14@gmail.com>
2024-04-21 13:16:56 -0300

    feat: created php course repository

---
+++ first-step/first-step.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo 'Hello from PHP!'; ?></h1>
+    <h1>Hey, this is my first .php file</h1>
+    <h1>A dice roll: <?php echo rand(1, 6) ?> </h1> 
+</body>
+</html>
